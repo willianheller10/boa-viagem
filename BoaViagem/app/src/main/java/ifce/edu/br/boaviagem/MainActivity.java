dd13c3bbe498a31d554606555c7f5723cf83945a
Beatriz <beatrizpnha@gmail.com>
2018-03-16 14:11:22 -0300

    Criando versão inicial do projeto

---
+++ BoaViagem/app/src/main/java/ifce/edu/br/boaviagem/MainActivity.java
@@ -0,0 +1,13 @@
+package ifce.edu.br.boaviagem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
